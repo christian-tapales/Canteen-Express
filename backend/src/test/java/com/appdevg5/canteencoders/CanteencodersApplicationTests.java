0c186fea9c8a263c7de8cc482c8ab5eb733b3628
Christian Kyle Tapales <tapaleskyle629@gmail.com>
2025-11-25 20:24:02 +0800

    Initial setup of Monorepo structure

---
+++ backend/src/test/java/com/appdevg5/canteencoders/CanteencodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdevg5.canteencoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteencodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
