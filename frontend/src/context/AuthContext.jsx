101eb4194c0f42bd3c3845333ee16fc986751a31
Christian Kyle Tapales <tapaleskyle629@gmail.com>
2025-11-27 14:33:01 +0800

    fixed the AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const useAuth = () => {
   return useContext(AuthContext);
